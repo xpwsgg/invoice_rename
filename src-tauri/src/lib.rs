38c1fe7dba266bf9508983a225b9982b547a6a37
xiao <xpwsgg@163.com>
2026-05-13 22:03:25 +0800

    feat: scaffold tauri 2 application shell

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
