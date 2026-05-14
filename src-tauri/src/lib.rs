1c36a4ed707609f4d63101763c1b0bdc292a062a
xiao <xpwsgg@163.com>
2026-05-14 21:21:21 +0800

    feat(app): prevent multi-instance via single-instance plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,8 +3,17 @@ mod error;
 mod pdf_parser;
 mod renamer;
 
+use tauri::Manager;
+
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![commands::rename_pdfs])
         .run(tauri::generate_context!())
